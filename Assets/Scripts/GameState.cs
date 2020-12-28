e6921ca684e8b3e38e3d4d9e44522cf51815166b
nedved <nedved8891@gmail.com>
2020-12-28 11:51:59 +0200

    Save

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -43,6 +43,5 @@ public class GameState : MonoBehaviour
         State.RegisterState(AppState.MainMenu, new MainMenuAppState());
         State.RegisterState(AppState.Game, new GameAppState());
         
-        
     }
 }
